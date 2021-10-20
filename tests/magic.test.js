01ff74b5ca451e95a6f2e7f776f584400b4fdb22
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-10-20 11:17:53 -0300

    Cria teste para a funçao magic - Exercicio 1

--- tests/magic.test.js
+++ tests/magic.test.js
@@ -4,17 +4,18 @@ const { trybeSimulator } = require('../simulator/simulator.js');
 const expected = {
   name: "Ancestor's Chosen",
   manaCost: '{5}{W}{W}',
-  types: [ 'Creature' ],
-  subtypes: [ 'Human', 'Cleric' ],
-  rarity: 'Uncommon'
+  types: ['Creature'],
+  subtypes: ['Human', 'Cleric'],
+  rarity: 'Uncommon',
 };
 
 describe(' Testa a função getMagicCard', () => {
-  it('Deve retornar um objeto com as propriedades esperadas', () => {
-    const getMagicCardSimulator = trybeSimulator(magic, 'getMagicCard');
-    await getMagicCardSimulator('130550');
+  it('Deve retornar um objeto com as propriedades esperadas', async () => {
+    expect.assertions(1);
 
-    // implemente seus testes aqui    
-   
+    const getMagicCardSimulator = trybeSimulator(magic, 'getMagicCard');
+    const response = await getMagicCardSimulator('130550');
+
+    expect(response).toEqual(expected);
   });
 });
