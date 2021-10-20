4d8e10cad2480de585258210e7490ca84a77ac83
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-10-20 13:13:36 -0300

    magic.test - Testa se na última posição do array favoriteCards existe um card com o a propriedade name e valor "Beacon of Immortality"

--- tests/magic.test.js
+++ tests/magic.test.js
@@ -3,21 +3,19 @@ const favoriteCards = require('../data/favoriteCards.js');
 
 const retrievesFavoriteCards = () => {
   // implemente sua função aqui
-}
+};
 
-describe(' Testa a função getMagicCard', () => { 
-
+describe(' Testa a função getMagicCard', () => {
   it('Testa se um novo card é adicionado a cada execução', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     await getMagicCard('130553');
-    expect(favoriteCards).toHaveLength(5)
+    expect(favoriteCards).toHaveLength(5);
+    expect(favoriteCards[4].name).toBe('Beacon of Immortality');
   });
 
   it('Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
     // expect.assertions();
-
     // expect(favoriteCards).toHaveLength(4);
     // implemente seus testes aqui
-
   });
 });
